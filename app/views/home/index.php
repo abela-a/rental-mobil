153c82f735ec179cc6f76688cb3bf38ba0fb4471
AbelArdhana <abelardhana96@gmail.com>
2019-10-11 20:24:09 +0800

    membuat landing page

---
+++ app/views/home/index.php
@@ -0,0 +1,44 @@
+<!-- AWAL LANDING -->
+<div class="mt-4">
+  <!-- JUMBOTRON -->
+  <div class="view jarallax jumbotron card card-image" style="height:100vh">
+    <!-- JARALLAX -->
+    <img src="<?= BASEURL ?>/img/assets/image4.jpg" alt="" class="jarallax-img">
+    <!-- MASK WARNA -->
+    <div class="mask flex-center rgba-indigo-strong">
+      <div class="text-white container px-4">
+        <div class="row">
+          <div class="col-md-5 col-sm-12">
+            <h3 style="font-weight:500;font-size:50px;">
+              <?= APP_NAME; ?> <?= APP_TYPE; ?>
+            </h3>
+            <span style="font-weight:100;font-size:29px;font-family:'Fira Code iScript'">
+              <i> Your Car Rental Solution</i>
+            </span>
+            <div id="about" class="pr-5">
+              <p class="mt-4">
+                Kami adalah penyedia layanan sewa mobil di Makassar yang sudah berpengalaman dalam menyediakan mobil berkualitas dengan harga yang murah sejak tahun 2019.
+              </p>
+              <br>
+              <p>
+                Kami siap melayani kebutuhan Anda selama di Makassar untuk tujuan wisata, mudik, urusan pekerjaan, pindahan, wisuda, keluar kota, dan berbagai kebutuhan yang Anda perlukan.
+              </p>
+              <br>
+              <p>
+                Kami selalu berkomitmen untuk memberikan pelayanan <b>terbaik</b> dengan mobil yang <b>terawat</b> dan sopir yang <b>kompeten</b>.
+              </p>
+            </div>
+            <div class="clear-fix mt-4">
+              <button class="btn btn-warning" style="color:black">Daftar</button>
+              <button class="btn btn-outline-white" style="color:black">login</button>
+            </div>
+          </div>
+          <div class="col-md-7 col-sm-12">
+            <img src="<?= BASEURL ?>/img/assets/landing.png" style="z-index: 100;position: absolute;height: 550px;right: -50px;bottom: -155px;">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- AKHIR LANDING -->
