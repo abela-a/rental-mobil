242d039e570b6a00b9c8491fb9ed277b0036badc
AbelArdhana <abelardhana96@gmail.com>
2019-10-11 22:00:20 +0800

    update section "mengapa memilih kami?"

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -29,8 +29,8 @@
               </p>
             </div>
             <div class="clear-fix mt-4">
-              <a class="btn btn-warning" href="<?= BASEURL; ?>/auth/login" style="color:black">Daftar</a>
-              <a class="btn btn-outline-white" href="<?= BASEURL; ?>/auth/registration" style="color:black">login</a>
+              <a class="btn btn-warning" href="<?= BASEURL; ?>/auth/registration" style="color:black">Daftar</a>
+              <a class="btn btn-outline-white" href="<?= BASEURL; ?>/auth/login" style="color:black">login</a>
             </div>
           </div>
           <div class="col-md-7 col-sm-12">
@@ -43,8 +43,49 @@
 </div>
 <!-- AKHIR LANDING -->
 
-<div class="container">
-  <!-- AWAL MEMILIH KAMI -->
+<div class="container position-relative">
 
+  <!-- AWAL MEMILIH KAMI -->
+  <section id="alasan" class="px-5 py-5 bg-white rounded shadow" style="margin-top:-50px;z-index:10;position:absolute;width:100%">
+
+    <h4 class="text-center h3">Mengapa Memilih Kami ?</h4>
+    <div class="row mt-5">
+      <!-- HARGA -->
+      <div class="col-md-4 col-sm-12">
+        <div class="text-center">
+          <i class="fas fa-money-bill-wave-alt fa-4x text-warning d-block mb-3"></i>
+          <span class="h5">Harga Terjangkau</span>
+          <p class="px-4 mt-3">
+            Harga sewa mobil terjangkau di Makassar hanya ada di perusahaan kami, jangan ragu untuk menentukan rencana sewa mobil di Makassar, satukan pilihan Anda juga termasuk layanan Sopir kami berpengalaman.
+          </p>
+        </div>
+      </div>
+
+      <!-- LAYANAN -->
+      <div class="col-md-4 col-sm-12">
+        <div class="text-center">
+          <i class="fas fa-headset fa-4x text-warning d-block mb-3"></i>
+          <span class="h5">Pelayanan Prima</span>
+          <p class="px-4 mt-3">
+            Costumer sevice kami siap melayani Anda kapan pun. Dan siap dengan media apapun, mulai dari via SMS, Whatsapp, E-mail, maupun telepon secara langsung.
+          </p>
+        </div>
+      </div>
+
+      <!-- KUALITAS -->
+      <div class="col-md-4 col-sm-12">
+        <div class="text-center">
+          <i class="fas fa-user-tie fa-4x text-warning d-block mb-3"></i>
+          <span class="h5">Kualitas Terbaik</span>
+          <p class="px-4 mt-3">
+            Perusahaan kami didukung dengan tim yang profesional dan dikelola oleh SDM pilihan serta unit mobil yang variatif, mulai dari city car, sampai mobil rombongan dan mobil mewah untuk pengantin.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
   <!-- AKHIR MEMILIH KAMI -->
+
+  <!-- AWAL DAFTAR MOBIL -->
+  <!-- AKHIR DAFTAR MOBIL -->
 </div>
