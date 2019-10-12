099b5ae324e8b817aacf0e2523a30aab24514bd0
AbelArdhana <abelardhana96@gmail.com>
2019-10-12 10:54:56 +0800

    membuat footer

--- app/views/templates/footerdashboard.php
+++ app/views/templates/footerdashboard.php
@@ -1,5 +1,5 @@
 <!-- FOOTER -->
-<footer class="mt-5 rounded-top unique-color-dark py-3">
+<footer class="mt-5 rounded-top unique-color-dark py-2">
   <div class="text-center">
     Copyright &copy; <b><?= APP_NAME ?></b> <?= APP_TYPE ?> <?= date('Y') ?>
   </div>
--- app/views/templates/footerhome.php
+++ app/views/templates/footerhome.php
@@ -1,6 +1,63 @@
 <!-- FOOTER -->
-<footer class="mt-5 rounded-top">
-  <div class="text-center">
-    Copyright &copy; <b>SMK Mutiara Ilmu</b> <?= date('Y') ?>
+<footer class="mt-5">
+  <div class="unique-color-dark py-4 rounded-top">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-3 col-sm-12">
+          <div class="clear-fix">
+            <img src="<?= BASEURL; ?>/img/assets/logo.png" width="70" height="70" class="align-middle float-left" alt="">
+            <span style="font-size:25px" class="font-weight-bold d-block"><?= APP_NAME; ?></span>
+            <span style="font-size:18px" class="font-weight-thin"><?= APP_TYPE; ?></span>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-12">
+          <div class="clear-fix" style="font-size:15px">
+            <div class="float-left">
+              <i class="fas fa-map-marker-alt fa-fw"></i>
+            </div>
+            <div class="font-weight-regular ml-3 float-left">
+              <span class="font-weight-thin mb-2 ">Lokasi Kami</span><br>
+              Jl. Goa Ria <br>
+              Pondok Asri 1 <br>
+              Blok A7 No.10
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-12">
+          <div class="clear-fix" style="font-size:15px">
+            <div class="float-left">
+              <i class="fas fa-envelope fa-fw"></i>
+            </div>
+            <div class="font-weight-regular ml-3 float-left">
+              <span class="font-weight-thin mb-2 ">Email Kami</span><br>
+              abelardhana96@gmail.com <br>
+              nrftrianis12@gmail.com
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3 col-sm-12">
+          <div class="clear-fix" style="font-size:15px">
+            <div class="float-left">
+              <i class="fas fa-phone fa-fw"></i>
+            </div>
+            <div class="font-weight-regular ml-3 float-left">
+              <span class="font-weight-thin mb-2 ">Kontak Kami</span><br>
+              0878-1697-3617<br>
+              0878-1753-5342
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
+
+  <div class="text-center indigo accent-2 py-2">
+    Copyright &copy; <b><?= APP_NAME ?></b> <?= APP_TYPE ?> <?= date('Y') ?>
+  </div>
+
 </footer>
