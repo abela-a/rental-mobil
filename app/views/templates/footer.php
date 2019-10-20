47f8f54ee35ea392a87057ee944d7947dd294fd9
AbelArdhana <abelardhana96@gmail.com>
2019-10-20 10:39:17 +0800

    fix datepicker

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -26,8 +26,10 @@
 <?= SweetAlert::SwalAlert() ?><?= SweetAlert::SwalToast() ?>
 <!-- MASK -->
 <script src="<?= BASEURL; ?>/vendor/MaterialBootstrap/js/jquery.mask.min.js"></script>
-<!-- DATEPICKER -->
-<script src="<?= BASEURL; ?>/vendor/DatePicker/datepicker.min.js"></script>
+<!--SOPIR -->
+<script src="<?= BASEURL; ?>/js/sopir.js"></script>
+<!--DATEPICKER -->
+<script src="<?= BASEURL; ?>/js/datepicker_sett.js"></script>
 <!-- MY SCRIPT -->
 <script src="<?= BASEURL; ?>/js/script.js"></script>
 <!-- GET -->
